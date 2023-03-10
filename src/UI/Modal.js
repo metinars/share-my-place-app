0c8201446bfc380004cab9c652d79c408b1df048
metinars <arsmetin90@gmail.com>
2023-03-10 13:48:55 +0300

    Hiding the Modal

--- src/UI/Modal.js
+++ src/UI/Modal.js
@@ -1,27 +1,39 @@
-class Modal {
+export class Modal {
     constructor(contentId, fallbackText) {
         this.fallbackText = fallbackText;
         this.contentTemplateEl = document.getElementById(contentId);
         this.modalTemplateEl = document.getElementById('modal-template');
-    };
+    }
 
     show() {
         if ('content' in document.createElement('template')) {
-            const modalElements = document.importNode(this.modalTemplateEl.content, true);
-            const modalElement = document.querySelector('.modal');
-            const backdropElement = document.querySelector('.backdrop');
-            const contentElement = document.importNode(this.contentTemplateEl.content, true);
+          const modalElements = document.importNode(
+              this.modalTemplateEl.content,
+              true
+          );
+          this.modalElement = modalElements.querySelector('.modal');
+          this.backdropElement = modalElements.querySelector('.backdrop');
+          const contentElement = document.importNode(
+              this.contentTemplateEl.content,
+              true
+          );
 
-            modalElement.appendChild(contentElement);
+          this.modalElement.appendChild(contentElement);
 
-            document.body.insertAdjacentElement('afterbegin', modalElement);
-            document.body.insertAdjacentElement('afterbegin', backdropElement);
+          document.body.insertAdjacentElement('afterbegin', this.modalElement);
+          document.body.insertAdjacentElement('afterbegin', this.backdropElement);
+      } else {
+          // fallback code
+          alert(this.fallbackText);
+      }
+    }
 
-        } else {
-            //  fallback code
-            alert(this.fallbackText);
+    hide() {
+        if (this.modalElement) {
+            document.body.removeChild(this.modalElement); // this.modalElement.remove()
+            document.body.removeChild(this.backdropElement);
+            this.modalElement = null;
+            this.backdropElement = null;
         }
-    };
-
-    hide() { };
-};
+    }
+}
