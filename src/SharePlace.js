ab5478093aa8729c7bb0093569f5cf57475814f2
metinars <arsmetin90@gmail.com>
2023-03-10 15:50:55 +0300

    Rendering a Map with Google Maps

--- src/SharePlace.js
+++ src/SharePlace.js
@@ -1,40 +1,54 @@
 import { Modal } from './UI/Modal';
+import { Map } from './UI/Map';
 
 class PlaceFinder {
     constructor() {
         const addressForm = document.querySelector('form');
         const locateUserBtn = document.getElementById('locate-btn');
 
-        locateUserBtn.addEventListener('click', this.locateUserHandler);
-        addressForm.addEventListener('submit', this.findAdressHandler);
+        locateUserBtn.addEventListener('click', this.locateUserHandler.bind(this));
+        addressForm.addEventListener('submit', this.findAddressHandler.bind(this));
+    }
+
+    selectPlace(coordinates) {
+        if (this.map) {
+            this.map.render(coordinates);
+        } else {
+            this.map = new Map(coordinates);
+        }
     }
 
     locateUserHandler() {
         if (!navigator.geolocation) {
             alert(
-                'Location feature is not avaliable in your browser - please use a more modern browser or manueally enter an address.'
+                'Location feature is not available in your browser - please use a more modern browser or manually enter an address.'
             );
             return;
         }
-        const modal = new Modal('loading-modal-content', 'Lodaing location - please wait!');
+        const modal = new Modal(
+            'loading-modal-content',
+            'Loading location - please wait!'
+        );
         modal.show();
         navigator.geolocation.getCurrentPosition(
-            succesResult => {
+            successResult => {
                 modal.hide();
-                const cordinates = {
-                    lat: succesResult.coords.latitude,
-                    lng: succesResult.coords.longitude
+                const coordinates = {
+                    lat: successResult.coords.latitude,
+                    lng: successResult.coords.longitude
                 };
-                console.log(cordinates);
-            }, error => {
+                this.selectPlace(coordinates);
+            },
+            error => {
                 modal.hide();
                 alert(
                     'Could not locate you unfortunately. Please enter an address manually!'
                 );
-            });
-    };
+            }
+        );
+    }
 
-    findAddressHandler() { };
-};
+    findAddressHandler() { }
+}
 
 const placeFinder = new PlaceFinder();
